6002a16b16ab92d7483e15822e22918c15c5bbd9
Tim Head <betatim@gmail.com>
2016-12-13 12:05:29 +0100

    Jupp

--- src/App.js
+++ src/App.js
@@ -1,59 +1,96 @@
 import React, { Component } from 'react';
 import DropZone from 'react-dropzone';
+import EXIF from 'exif-js';
 import logo from './logo.svg';
 import './App.css';
 
 
+class ExifRow extends React.Component {
+  render() {
+    const val = this.props.value;
+    return (
+      <li>
+        {this.props.name}:  <i>{val.toString()}</i>
+      </li>
+    );
+  }
+}
+
 
 class Image extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = {image_src: null}
+    super(props);
+    this.state = { image_src: null, exif: [] };
   }
 
   componentDidMount() {
-    var reader = new FileReader();
+    const reader = new FileReader();
     reader.onloadend = (e) => {
-      this.setState({image_src: e.target.result})
-      console.log(e.target)
-    }
-    reader.onloadend.bind(this)
+      this.setState({ image_src: e.target.result });
+    };
+    reader.onloadend.bind(this);
     reader.readAsDataURL(this.props.image);
+
+    const reader2 = new FileReader();
+    reader2.onloadend = (e) => {
+      this.setState({ exif: EXIF.readFromBinaryFile(e.target.result) });
+    };
+    reader2.onloadend.bind(this);
+    reader2.readAsArrayBuffer(this.props.image);
   }
 
-  render () {
+  render() {
     const liStyle = {
       listStyleType: 'none',
       margin: '20px 20px 0px 20px',
       padding: '20px',
-      backgroundColor: '#ccc'
-    }
+      backgroundColor: '#fafafa',
+    };
+    const imgStyle = {
+      maxWidth: '80%',
+    };
+    let exifItems = [];
+    Object.keys(this.state.exif).forEach((key) => {
+      const val = this.state.exif[key];
+      exifItems.push(<ExifRow name={key} value={val} />);
+      //console.log(exifItems);
+    });
     return (
       <li style={liStyle}>
         {this.state.image_src ?
           <div>
-            <div>{this.props.image.name}</div>
-            <img src={this.state.image_src} alt="sdg" />
+            <img src={this.state.image_src} alt="sdg" style={imgStyle} />
+            {this.state.exif ?
+              <ul>
+                {exifItems}
+              </ul>
+              :
+              <div>No metadata.</div>
+            }
           </div>
           :
           <div>Nope</div>
         }
       </li>
-      )
+    );
   }
 }
+
+
 class ImageList extends React.Component {
   render() {
-    var items = [];
-    this.props.images.forEach(function(image) {
-      items.push(<Image
-        image={image}
-        key={image.name + image.lastModified} />);
+    let items = [];
+    this.props.images.forEach((image) => {
+      items.push(
+        <Image
+          image={image}
+          key={image.name + image.lastModified}
+        />);
     });
     return (
       <div>
         <p>Analysed images:</p>
-        <ul style={{padding: 0, margin: 0}}>
+        <ul style={{ padding: 0, margin: 0 }}>
           {items}
         </ul>
       </div>
@@ -64,12 +101,12 @@ class ImageList extends React.Component {
 
 class SearchBar extends Component {
   render() {
-      return (
-        <form>
-          <input type="text" placeholder="Search..." />
-        </form>
-      );
-    }
+    return (
+      <form>
+        <input type="text" placeholder="Search..." />
+      </form>
+    );
+  }
 }
 
 
@@ -92,40 +129,43 @@ class App extends Component {
     this.handleDrop = this.handleDrop.bind(this)
   }
 
-  handleDrop(files) {
-    this.setState({files: files});
-  }
-
   // static data for testing
+  /*
   componentDidMount() {
-    var IMAGES = []
-    var xhr = new XMLHttpRequest();
+    let IMAGES = []
+    let xhr = new XMLHttpRequest();
     xhr.open('GET', 'http://localhost:3000/30_flickr-cats.jpg');
-    xhr.responseType = "blob"
-    xhr.send()
+    xhr.responseType = 'blob';
+    xhr.send();
     xhr.onloadend = (e) => {
       IMAGES.push(
-        new File([e.target.response],
-           'cat.jpg',
-           {type: e.target.response.type})
-         )
+        new File(
+          [e.target.response],
+          'cat.jpg',
+          { type: e.target.response.type }),
+         );
       IMAGES.push(
         new File([e.target.response],
            'cat2.jpg',
-           {type: e.target.response.type}))
-      this.setState({files: IMAGES})
-    }
+           { type: e.target.response.type }));
+      this.setState({ files: IMAGES });
+    };
+  }
+  */
+
+  handleDrop(files) {
+    this.setState({ files: files });
   }
 
   render() {
     const DZstyle = {
-        width: '80%',
-        height: 200,
-        borderWidth: 2,
-        borderColor: '#666',
-        borderStyle: 'dashed',
-        borderRadius: 5
-      };
+      width: '80%',
+      height: 200,
+      borderWidth: 2,
+      borderColor: '#666',
+      borderStyle: 'dashed',
+      borderRadius: 5,
+    };
     return (
       <div className="App">
         <div className="App-header">
@@ -144,10 +184,10 @@ class App extends Component {
         <hr />
         {this.state.files ?
           <div>
-            <FilterableImageList images={this.state.files}/>
+            <FilterableImageList images={this.state.files} />
           </div>
           :
-          <div>Waiting</div>
+          ''
         }
       </div>
     );
